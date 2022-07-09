3d1b30dcd493adafb2005ba11118149b6899f976
pradhyankita <rapenzelpb@gmail.com>
2022-07-09 06:52:45 +0530

    modified UC1

--- src/com/BridgeLabz/EmployeeWage/EmployeeWage.java
+++ src/com/BridgeLabz/EmployeeWage/EmployeeWage.java
@@ -5,5 +5,10 @@ public class EmployeeWage {
         System.out.println("Welcome to Employee Wge Program");
         int employeePresent= (int)Math.floor(Math.random()*10)%2;
         System.out.println(employeePresent);
+        if(employeePresent==1)
+            System.out.println("Employee is present");
+        else
+            System.out.println("Employee is absent");
+        System.out.println("UC1 done");
     }
 }
