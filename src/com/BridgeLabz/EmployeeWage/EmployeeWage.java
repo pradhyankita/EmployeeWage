098892ccdb8ee54456a740e69cddf7b6d2aba64f
pradhyankita <rapenzelpb@gmail.com>
2022-07-09 06:29:50 +0530

    adding 1st UC

--- src/com/BridgeLabz/EmployeeWage/EmployeeWage.java
+++ src/com/BridgeLabz/EmployeeWage/EmployeeWage.java
@@ -2,6 +2,8 @@ package com.BridgeLabz.EmployeeWage;
 
 public class EmployeeWage {
     public static void main(String args[]){
-        System.out.println("hi");
+        System.out.println("Welcome to Employee Wge Program");
+        int employeePresent= (int)Math.floor(Math.random()*10)%2;
+        System.out.println(employeePresent);
     }
 }
