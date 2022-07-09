8df174065cf9b7c1581e2405e11bd91cb08da64e
pradhyankita <rapenzelpb@gmail.com>
2022-07-09 08:54:50 +0530

    Adding UC6

--- src/com/BridgeLabz/EmployeeWage/EmployeeWage.java
+++ src/com/BridgeLabz/EmployeeWage/EmployeeWage.java
@@ -14,27 +14,38 @@ public class EmployeeWage {
 
     public static void main(String args[]) {
         System.out.println("Welcome to Employee Wge Program");
-        int dailyWage = 0;
-        int day = 0;
-        while (day <= TOTAL_WORKING_DAYS) {
+        int dailyWage = 1;
+        int day = 1;
+        int workingHours=0;
+        int totalWage=0;
+        while (day <= TOTAL_WORKING_DAYS && workingHours < TOTAL_WORKING_DAYS) {
             int empRandom = (int) Math.floor(Math.random() * 10) % 3;
             switch (empRandom) {
                 case IS_FULL_TIME:
                     System.out.println("Employee is present Full time");
                     EMPLOYEE_PRESENT = 20;
                     dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+                    workingHours+=FULL_DAY_HOUR;
                     break;
                 case IS_PART_TIME:
                     System.out.println("Employee is present Part time");
                     EMPLOYEE_PRESENT = 20;
                     dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
+                    workingHours+=PART_TIME_HOUR;
                     break;
                 default:
                     System.out.println("Employee is absent");
             }
-            System.out.println("Daily Wage for #"+day + "=>" + dailyWage);
-            //System.out.println("UC5 done");
+
+            System.out.println("Daily Wage for #" + day + "=>" + dailyWage);
+            totalWage+=dailyWage;
             day++;
+
         }
+        System.out.println("Total Working hours => " +workingHours);
+        System.out.println("Total Wage => " +totalWage);
+        System.out.println("Total Days =>" +day);
+        System.out.println("UC6 done");
     }
+
 }
